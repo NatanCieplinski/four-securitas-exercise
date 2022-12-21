f552f20fb5fef9ff35363ab29e604b00d9c82870
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2022-12-21 07:55:31 +0100

    Add filtering and sorting logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,91 @@
+import { useEffect, useState } from 'react'
+import { Character } from 'rickmortyapi/dist/interfaces'
+import { Search } from 'tabler-icons-react'
 import { useCharactersQuery } from './api/characters.queries'
 import { CharacterCard } from './components/CharacterCard'
 
 function App() {
-  const { data, isLoading } = useCharactersQuery()
+  const { data: characters, isLoading } = useCharactersQuery()
+  const localeFavorites =
+    JSON.parse(
+      (localStorage.getItem('favoriteCharactersIds') ?? []) as string
+    ) ?? []
+
+  const [searchFilter, setSearchFilter] = useState('')
+  const [favoriteCharactersIds, setFavoriteCharactersIds] =
+    useState<number[]>(localeFavorites)
+
+  useEffect(() => {
+    localStorage.setItem(
+      'favoriteCharactersIds',
+      JSON.stringify(favoriteCharactersIds)
+    )
+  }, [favoriteCharactersIds])
+
+  const filteredCharacters =
+    characters?.results?.filter((character) =>
+      character.name.toLowerCase().includes(searchFilter)
+    ) ?? []
+  const filteredCharactersIds = filteredCharacters.map(
+    (character) => character.id
+  )
+
+  const set = new Set(filteredCharactersIds)
+
+  const filteredAndFavoritesCharactersIds = favoriteCharactersIds.filter(
+    (item) => set.has(item)
+  )
+
+  const sortedCharactersIds = [
+    ...new Set([
+      ...filteredAndFavoritesCharactersIds,
+      ...filteredCharactersIds,
+    ]),
+  ]
+
+  const sortedAndFilteredCharacters = sortedCharactersIds.map(
+    (characterId) =>
+      filteredCharacters.find(
+        (character) => character.id === characterId
+      ) as Character
+  )
 
   if (isLoading) {
     return <div>Loading...</div>
   }
 
   return (
-    <div className="flex flex-col gap-8 justify-center items-center h-screen text-white bg-gray-800">
-      {data?.results.map((character) => (
-        <CharacterCard character={character} key={character.id} />
-      ))}
+    <div className="overflow-scroll justify-center items-center p-20 space-y-20 h-screen bg-gray-800">
+      <SearchBar onChange={(searchQuery) => setSearchFilter(searchQuery)} />
+      <div className="flex flex-wrap gap-10">
+        {sortedAndFilteredCharacters.map((character) => (
+          <CharacterCard
+            key={character.id}
+            character={character}
+            isFavorite={favoriteCharactersIds.includes(character.id)}
+            onAddToFavorite={() => {
+              setFavoriteCharactersIds([...favoriteCharactersIds, character.id])
+            }}
+            onRemoveFromFavorite={() => {
+              setFavoriteCharactersIds(
+                favoriteCharactersIds.filter((id) => id !== character.id)
+              )
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+function SearchBar({ onChange }: { onChange: (value: string) => void }) {
+  return (
+    <div className="flex gap-2 p-2 text-black bg-white rounded-lg">
+      <Search />
+      <input
+        className="w-full h-full rounded-lg outline-none"
+        onChange={(e) => onChange(e.target.value)}
+      />
     </div>
   )
 }
--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,5 +1,43 @@
+import classNames from 'classnames'
 import { Character } from 'rickmortyapi/dist/interfaces'
 
-export function CharacterCard({ character }: { character: Character }) {
-  return <div>{JSON.stringify(character)}</div>
+export function CharacterCard({
+  character,
+  isFavorite,
+  onAddToFavorite,
+  onRemoveFromFavorite,
+}: {
+  character: Character
+  isFavorite: boolean
+  onAddToFavorite: () => void
+  onRemoveFromFavorite: () => void
+}) {
+  return (
+    <div
+      className={classNames(
+        'flex flex-col gap-10 p-10 text-black rounded-2xl',
+        isFavorite ? 'bg-blue-300' : 'bg-white'
+      )}
+    >
+      <div className="flex gap-8">
+        <div>
+          <img
+            src={character.image}
+            alt={character.name}
+            className="aspect-square h-28 rounded-full"
+          />
+        </div>
+        <div>
+          <div>{character.status}</div>
+          <div>{character.episode.length}</div>
+        </div>
+      </div>
+      <div className="font-bold">{character.name}</div>
+      {isFavorite ? (
+        <button onClick={onRemoveFromFavorite}>Remove to favorites</button>
+      ) : (
+        <button onClick={onAddToFavorite}>Add to favorites</button>
+      )}
+    </div>
+  )
 }
